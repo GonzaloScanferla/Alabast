090a8900469eba37ffd039cdd32e9eac5633a7e3
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-11-26 16:09:25 +0100

    common.middleware file renamed to access.middleware

--- BackEnd/src/routes/api.js
+++ BackEnd/src/routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-const {checkToken} = require ('../middlewares/common.middleware')
+const {checkToken} = require ('../middlewares/access.middleware')
 
 router.use ('/register', require ('./api/register'))
 router.use ('/login', require ('./api/login'))
