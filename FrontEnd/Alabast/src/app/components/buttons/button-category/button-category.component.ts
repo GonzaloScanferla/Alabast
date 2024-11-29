98e8220b216836fbe13be8257e11329094aff885
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-11-29 06:33:46 +0100

    Navbar component and subcomponents created and set

---
+++ FrontEnd/Alabast/src/app/components/buttons/button-category/button-category.component.ts
@@ -0,0 +1,54 @@
+import { Component, HostListener } from '@angular/core';
+
+interface ICategory {
+  text: string,
+  icon: string,
+  expanded: boolean
+}
+
+@Component({
+  selector: 'app-button-category',
+  imports: [],
+  templateUrl: './button-category.component.html',
+  styleUrl: './button-category.component.css'
+})
+export class ButtonCategoryComponent {
+
+  buttonContent: ICategory [] = [
+    {
+      text: "Fires i Mercats",
+      icon: "storefront",
+      expanded: false
+    },
+    {
+      text: "Gastronomia",
+      icon: "grocery",
+      expanded: false
+    },
+    {
+      text: "Experiències",
+      icon: "spa",
+      expanded: false
+    },
+    {
+      text: "Activitats",
+      icon: "groups",
+      expanded: false
+    },
+  ] 
+
+  openMenu(item: ICategory, event: Event): void {
+    event.stopPropagation(); // Prevent the click event from propagating to the document 
+    // Close all other menus
+    this.buttonContent.forEach(i => {
+      if (i !== item) { i.expanded = false; }
+    });
+    // Toggle the clicked menu
+    item.expanded = !item.expanded;
+  }
+  @HostListener('document:click', ['$event']) closeMenus(event: Event): void {
+      this.buttonContent.forEach(item => {
+      item.expanded = false;
+    })
+  }
+}
---
+++ FrontEnd/Alabast/src/app/components/buttons/button-important/button-important.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-important',
+  imports: [],
+  templateUrl: './button-important.component.html',
+  styleUrl: './button-important.component.css'
+})
+export class ButtonImportantComponent {
+
+}
