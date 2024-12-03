438445e00b291ece7521e85e4cdb62f976fe8c44
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-12-03 11:25:35 +0100

    card-mini and card-micro components created

---
+++ FrontEnd/Alabast/src/app/components/card-micro/card-micro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-micro',
+  imports: [],
+  templateUrl: './card-micro.component.html',
+  styleUrl: './card-micro.component.css'
+})
+export class CardMicroComponent {
+
+}
